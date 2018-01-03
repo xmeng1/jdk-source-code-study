4efcfb6f7f2d2dedda561c06bbe6f391e7e2a67e
Xin Meng <x.meng@outlook.com>
2018-01-03 22:00:11 +0000

    make a copy of HashMap source code and add comment

--- src/test/java/HashMapStudy.java
+++ src/test/java/HashMapStudy.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * User:    mengxin
  * Date:    02/01/2018
@@ -10,6 +13,33 @@
  */
 public class HashMapStudy {
     public static void main(String[] arg) {
+        Map<String,Integer> test = new HashMap<>();
+
+        test.put("KING", 100);
+        test.put("CLARK", 99);
+        test.put("BLAKE", 10);
+        test.put("FORD", 110);
+        test.put("SMITH", 10);
+        test.put("WARD", 100);
+        test.put("JONES", 90);
+
+        System.out.print("previous value " + test.put("KING", 1010) + "\n");
+        System.out.print("previous value " + test.put("CLARK", 919) + "\n");
+        System.out.print("previous value " + test.put("BLAKE", 110) + "\n");
+        System.out.print("previous value " + test.put("FORD", 1110) + "\n");
+        System.out.print("previous value " + test.put("SMITH", 110) + "\n");
+        System.out.print("previous value " + test.put("WARD", 1010) + "\n");
+        System.out.print("previous value " + test.put("JONES", 910) + "\n");
+
+        System.out.print(test);
+
+        test.get("KING");
+        test.get("CLARK");
+        test.get("BLAKE");
+        test.get("FORD");
+        test.get("SMITH");
+        test.get("WARD");
+        test.get("JONES");
 
     }
 }
